f23be3b0e44ce72360da667036281d0f656842b6
ChaoticDragonHead <136110505+ChaoticDragonHead@users.noreply.github.com>
2025-12-01 03:06:32 -0800

    ran colcon build command to generate install, build, and log folders

---
+++ build/fool_bot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jo-jo/ros2_ws/src/fool_bot/fool_bot_description
+# Build directory: /home/jo-jo/ros2_ws/build/fool_bot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
