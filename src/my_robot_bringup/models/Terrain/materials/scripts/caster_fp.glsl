412571faa1b122fadfea03916e5d20605db8cd9d
Jo-Jo <136110505+ChaoticDragonHead@users.noreply.github.com>
2025-12-01 11:40:37 -0800

    added backup model files so that potatoland runs

---
+++ src/my_robot_bringup/models/Terrain/materials/scripts/caster_fp.glsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:86818f35f5b6b7e99fa4dd276c6b1090fae157a7b95d199f877b43a5bb1a394f
+size 242
---
+++ src/my_robot_bringup/models/Terrain/materials/scripts/caster_vp.glsl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed8df153446011e1d7f77c7c12f5387de99a6e3216f98661ed96c59a8b8f1d84
+size 290
